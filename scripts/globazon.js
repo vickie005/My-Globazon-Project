c6826096014fa7ac64600c7d25763085215d6be5
vickie005 <mwendwavictory29@gmail.com>
2025-06-07 13:15:50 +0300

    made add to cart button interactive

--- scripts/globazon.js
+++ scripts/globazon.js
@@ -1,7 +1,7 @@
-const products = [
+/*const products = [
   {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
     rating: {
       stars: 4.5,
       count: 87,
@@ -9,8 +9,8 @@ const products = [
     priceCents: 1090,
   },
   {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate size Basketball",
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate size Basketball',
     rating: {
       stars: 4,
       count: 127,
@@ -18,14 +18,23 @@ const products = [
     priceCents: 2095,
   },
   {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
     rating: {
       stars: 4.5,
       count: 56
     },
     priceCents: 799
+  }, {
+    image: 'images/products/black-2-slot-toaster.jpg',
+    name: '2 Slot Toaster - Black',
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 1899
   }];
+  */
 
 let productsHTML = '';
   
@@ -51,7 +60,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${product.priceCents /100}
+            $${(product.priceCents /100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -76,12 +85,37 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id ="${product.id}">
             Add to Cart
           </button>
         </div>
    `;
 });
 
-document.querySelector('.js-products-grid').
-  innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1,
+        });
+      } 
+      console.log(cart);
+    });
+  })  
