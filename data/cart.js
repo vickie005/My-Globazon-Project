c4dc9d34ba80575d024d24e960a934326cde0266
vickie005 <mwendwavictory29@gmail.com>
2025-06-17 13:00:30 +0300

    save the data, generate and regenerate HTML when updating delivery option.

--- data/cart.js
+++ data/cart.js
@@ -5,12 +5,13 @@ if (!cart) {
    {
      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
      quantity: 2,
+     deliveryOptionId: '1'
    },
    {
      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
      quantity: 1,
-   },
- ];
+     deliveryOptionId: '2'
+   }];
 }
   
 function saveToStorage() {
@@ -32,6 +33,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
@@ -47,6 +49,20 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
