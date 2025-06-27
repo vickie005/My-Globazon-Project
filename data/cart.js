bd3d552e790d8efd643f32792b8acae70b650303
vickie005 <mwendwavictory29@gmail.com>
2025-06-27 10:52:22 +0300

    testing orderSummary section with Jasmine framework

--- data/cart.js
+++ data/cart.js
@@ -6,8 +6,7 @@ export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
 
   if (!cart) {
-    cart = [
-      {
+    cart = [{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2,
         deliveryOptionId: '1'
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -23,6 +23,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
     <div class="cart-item-container
+     js-cart-item-container
       js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Delivery date: ${dateString}
@@ -39,7 +40,8 @@ export function renderOrderSummary() {
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity
+                  js-product-quantity-${matchingProduct.id}">
                     <span>
                       Quantity: <span class="quantity-label">${
                         cartItem.quantity
@@ -48,9 +50,9 @@ export function renderOrderSummary() {
                     <span class="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
-                      matchingProduct.id
-                    }">
+                    <span class="delete-quantity-link link-primary js-delete-link
+                    js-delete-link-${matchingProduct.id}" 
+                    data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
@@ -109,7 +111,6 @@ export function renderOrderSummary() {
     link.addEventListener("click", () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
 
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
