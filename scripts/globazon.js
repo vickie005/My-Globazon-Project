b1186457802c06c46e50e315e20bf973ca41d6da
vickie005 <mwendwavictory29@gmail.com>
2025-07-11 14:41:46 +0300

    Load products from the backend

--- scripts/globazon.js
+++ scripts/globazon.js
@@ -1,11 +1,15 @@
 import { cart, addToCart } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products,loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid() {
+
+  let productsHTML = '';
   
-products.forEach((product) => {
-  productsHTML += `
+  products.forEach((product) => {
+    productsHTML += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -26,7 +30,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-           ${product.getPrice()}
+          ${product.getPrice()}
           </div>
 
           <div class="product-quantity-container">
@@ -58,26 +62,27 @@ products.forEach((product) => {
             Add to Cart
           </button>
         </div>
-   `;
-});
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
+  `;
   });
 
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-}
-  
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-      addToCart(productId);
-      updateCartQuantity();
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
     });
-  })  
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
+  
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+    });
+  })
+}
