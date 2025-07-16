fe042d3b5b345f7c8b1e5bd288bd3f33f865a9fd
vickie005 <mwendwavictory29@gmail.com>
2025-07-16 10:27:52 +0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -103,6 +103,8 @@ export function loadProductsFetch() {
     });
 
     console.log("load products");
+  }).catch((error) => {
+    console.log("Unexpected error. Please try again later.");
   });
 
   return promise;
@@ -128,6 +130,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
